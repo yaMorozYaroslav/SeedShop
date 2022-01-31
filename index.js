84c8183158de71e3a5af5388b87f99789c94f7ab
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-01-31 16:03:38 +0100

    second

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const yaroslav = [app,]
+const breakfast = {first: 'tea',
+                    second: 'bread', 
+                    third:'steak'};
+const lunch = {
+	first: 'cofee',
+	second: 'soup'
+};
+function longer(menu1, menu2){
+  if(menu1.first.length<menu2.first.length){
+  	return menu1
+  }else{ return menu2
+  }
+}
+console.log(longer(breakfast, lunch))
