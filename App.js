49e4b07d52bb5b37a9a389b9f04785ff0d5b811f
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-02-08 20:50:21 +0100

    yaro

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
-function App() {
-  return (
-    <div className="App">
-      <script src="index.js"></script>
-    </div>
-  );
-}
-
+import React, {Component}from'react';
+import Component1 from './functional/comp1'
+class App extends Component{
+	
+render(){
+	return(
+		<div className="App">
+		 <Component1 />
+		</div>
+		);
+      }
+    }
 export default App;
---
+++ functional/comp1.js
@@ -0,0 +1,7 @@
+import React from 'react';
+const Component1 = props => (
+ <div>
+ Component 2
+ </div>
+	);
+export default Component1;
