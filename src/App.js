0d6776a44bd89ffe5f5846f0dfbf66f9f7903531
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-02-09 18:58:50 +0100

    third

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, {Component}from'react';
-import Component1 from './functional/comp1'
 import Container1 from './containers/cont1'
 class App extends Component{
-	
-render(){
-	return(
-	<div>
-	<Container1 />
-	</div>
-	)}}
+
+  render(){
+   return(
+  <div>
+  	<Container1 />
+   </div>
+		 	)}
+		 }
 export default App;
--- src/containers/cont1.js
+++ src/containers/cont1.js
@@ -2,30 +2,23 @@ import React, {Component} from 'react';
 import Component1 from '../functional/comp1'
 
 class Container1 extends Component {
-  constructor(props){
-  	super(props)
-  	this.state={
-  		stateprop1: "Our Initial State",
-  		stateprop2: 5
-  	}}
- changeState=()=>(
-  this.setState({stateprop2: this.state.stateprop2+1}
-  	))
- changeState2=()=>(
-  this.setState({stateprop1: this.state.stateprop1+"L"}
-  	))
+  Arr1 = [
+  {id:1, text:'text 1', number:1},
+  {id:2, text:'text 2', number:2},
+  {id:3, text:'text 3', number:3},
+  {id:4, text:'text 4', number:4},
+  ]
+  RenderListItem=(props)=>(
+  	<div>
+  	{props.item.text}
+  	<p>{props.item.number}</p>
+  	</div>
+  	)
 	render(){
 		return(
 			<div>
-			<button 
-			onClick={()=>this.changeState()}>Change State
-			</button>
-			<br />
-		   <button 
-			onClick={()=>this.changeState2()}>Change State
-		   </button>
-		     <br />
-		     <Component1 prop1={this.state.stateprop1}/>
+    {this.Arr1.map((item)=>( 
+    	       <this.RenderListItem item={item}/> ) )}
 			</div>
 			)}
 }
