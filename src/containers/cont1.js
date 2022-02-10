b4771c9fb65edad72118344fbc320d2d653534fb
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-02-10 15:20:05 +0100

    eve

--- src/containers/cont1.js
+++ src/containers/cont1.js
@@ -2,31 +2,11 @@ import React, {Component} from 'react';
 import Component1 from '../functional/comp1'
 
 class Container1 extends Component {
-    constructor(props){
-    	super(props)
-    	this.handleChange=this.handleChange.bind(this)
-    	this.handleSubmit=this.handleSubmit.bind(this)
-    }
-	state={
-		value: ''
-	}
-
-	handleChange(event){
-		this.setState({value:event.target.value})
-	}
-	handleSubmit(event){
-		event.preventDefault()
-        console.log(this.state.value)
-		}
+    
 	render(){
 		return(
 		 <div>
-		 {this.state.value}
-		 <form onSubmit={this.handleSubmit}>
-    <label> Name </label>
-    <input id="name" onChange={this.handleChange} type="text" />
-    <button type="submit"> Submit </button>
-    </form>
+		 
 			</div>
 			)}
 }
