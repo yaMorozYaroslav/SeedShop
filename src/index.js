b7426052eeb993f061d339957b436d62ec0c12c2
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-02-11 15:41:48 +0100

    first

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import './index.css';
+import {BrowserRouter as Router, Routes, Route} 
+                               from 'react-router-dom';
 
 ReactDOM.render(
-	<React.StrictMode>
-	  <Home />
-	</React.StrictMode>,
+	<Router>
+	  <Routes>
+	    <Route path="/" element={<Home />}/>
+	  </Routes>
+	</Router>,
   document.getElementById('root'));
 
   function Home(){
   	return(
   	<div>
   	  <h1>Home route</h1>
-  	</div>)
+  	</div>
+  	);
+  }
+  function Learn(){
+  	return(
+  	<div>
+  	  <h1>Learn</h1>
+  	  <h4>All courses are listed here</h4>
+  	</div>
+  	);
   }
