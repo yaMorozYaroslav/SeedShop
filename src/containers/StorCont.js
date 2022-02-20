2da3242f2fb15210ea5b7ec3e7605f1a0c44d501
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-02-20 15:55:45 +0100

    33

--- src/containers/StorCont.js
+++ src/containers/StorCont.js
@@ -5,18 +5,19 @@ import {GlobalStyle, StoriesContainerWrapper}
                       from '../styles/StorContStyl';
 import {useInfiniteScroll} from '../hooks/useInfiniteScroll';
  export const StorCont = () =>{
+  const {count} = useInfiniteScroll();
 	const [storyIds, setStoryIds] = useState([]);
-  const test = useInfiniteScroll();
+  //const test = useInfiniteScroll();
 	useEffect(()=>{
 		getStoryIds().then(data=>setStoryIds(data));
-		getStory(30388152).then(data => console.log(data));
-	}, []);
+    console.log('count', count);
+	}, [count]);
      return (
       <>
       <GlobalStyle />
       <StoriesContainerWrapper data-test-id="stories-container">
         <h1>Hacker News Stories</h1>
-  {storyIds.map(storyDi=>
+  {storyIds.slice(0, count).map(storyDi=>
   	        (<Story key={storyDi} storyDi={storyDi}/>
                  ))}
       </StoriesContainerWrapper>
