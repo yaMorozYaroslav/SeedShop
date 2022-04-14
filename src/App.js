3bfc0433d6bac32d74baa2f4c1b46d4d2f644fb7
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-04-14 19:46:24 +0200

    02

--- src/App.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import {StorCont} from './containers/StorCont';
-export const App = () => <StorCont/>;
-
--- src/utils/selectFields.js
+++
@@ -1,3 +0,0 @@
-export const selectFields = ({id, by, url, time, title}={})=>({
-	id, by, url, time, title,
-})
