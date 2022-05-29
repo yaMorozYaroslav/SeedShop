24a82947c5363c6fdc90c59946c11bbae5a041eb
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-05-29 09:11:49 +0200

    932

---
+++ front/src/App.js
@@ -0,0 +1,12 @@
+import  React from 'react'
+import {Button} from './comps/Button/Button'
+
+export function App(){
+	return(
+       <div className="App">
+         <header className="App-header">
+          <Button label='Click me, please' />
+         </header>
+       </div>
+ 		)
+}
