050f8d8d1082fb6cb8c551af08c97eab4546abd4
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-05-31 01:19:14 +0200

    59

--- front/src/comps/Todo.test.js
+++ front/src/comps/Todo.test.js
@@ -1,4 +1,5 @@
 import {render, screen, cleanup} from '@testing-library/react'
+import renderer from 'react-test-renderer'
 import {Todo} from './Todo'
 import {toBeInTheDocument} from '@testing-library/jest-dom'
 
@@ -12,6 +13,7 @@ test('should render non-completed todo component',()=>{
 	const todoElement = screen.getByTestId('todo-1')
 	expect(todoElement).toBeInTheDocument()
 	expect(todoElement).toHaveTextContent('wash')
+	expect(todoElement).not.toContainHTML('<strike>')
 })
 test('should render completed todo component',()=>{
 	const todo = {id:2, title: 'wash car', completed: true}
@@ -19,5 +21,9 @@ test('should render completed todo component',()=>{
 	const todoElement = screen.getByTestId('todo-2')
 	expect(todoElement).toBeInTheDocument()
 	expect(todoElement).toHaveTextContent('wash')
-	expect(todoElement).toContainHTML('<strike>')
+})
+test('matches snapshot', ()=>{
+	const todo = {id:1, title: 'wash dishes', completed: false}
+	const tree = renderer.create(<Todo todo={todo}/>).toJSON()
+	expect(tree).toMatchSnapshot()
 })
