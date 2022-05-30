e844c5289360edc336bb5d98765e08ebd702f573
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-05-31 01:38:22 +0200

    030

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,13 @@
-import  React from 'react'
-import {Todo} from './comps/Todo'
+//import './App.css'
+import React from 'react'
+import {Button} from './comps/Button'
+import {Search} from './comps/Search'
 
 export function App(){
-	const todos = [
-       {id: 1, title: 'wash dishes', completed: false},
-       {id: 2, title: 'make  dinner', completed: true}
-	]
 	return(
-       <div className="App">
-          {todos.map((todo)=>{
-          	return(<Todo todo={todo}/>)
-          })}
+       <div className='App'>
+         <Button />
+         <Search />
        </div>
- 		)
+		)
 }
