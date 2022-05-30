e89a9d132c6c455021ad6f3cc21eeadbb82c0e76
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-05-30 08:55:04 +0200

    732

--- front/src/comps/Todo.test.js
+++ front/src/comps/Todo.test.js
@@ -2,7 +2,7 @@ import {render, screen, cleanup} from '@testing-library/react'
 import {Todo} from './Todo'
 import {toBeInTheDocument} from '@testing-library/jest-dom'
 
-test('should render todo component',()=>{
+test('should render non-completed todo component',()=>{
 	const todo = {id:1, title: 'wash dishes', completed: false}
 	render(<Todo todo={todo}/>)
 	const todoElement = screen.getByTestId('todo-1')
