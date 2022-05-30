63e7c58d0cccbcc34a1537f28ce1950259c1924f
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-05-30 08:58:01 +0200

    76

--- front/src/comps/Todo.test.js
+++ front/src/comps/Todo.test.js
@@ -9,3 +9,10 @@ test('should render non-completed todo component',()=>{
 	expect(todoElement).toBeInTheDocument()
 	expect(todoElement).toHaveTextContent('wash')
 })
+test('should render completed todo component',()=>{
+	const todo = {id:2, title: 'wash car', completed: true}
+	render(<Todo todo={todo}/>)
+	const todoElement = screen.getByTestId('todo-2')
+	expect(todoElement).toBeInTheDocument()
+	expect(todoElement).toHaveTextContent('wash')
+})
