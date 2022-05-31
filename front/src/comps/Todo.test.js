54b6f6d43200b06410734888cee13a0b07d4cf05
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-05-31 18:07:47 +0200

    976

--- front/src/comps/Todo.test.js
+++ front/src/comps/Todo.test.js
@@ -1,8 +1,9 @@
 import {render, screen, cleanup} from '@testing-library/react'
 import renderer from 'react-test-renderer'
+
 import {Todo} from './Todo'
 import {toBeInTheDocument} from '@testing-library/jest-dom'
-//
+
 afterEach(()=>{
 	cleanup()
 })
