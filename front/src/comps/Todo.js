cbce9111f2d21b0f26735184bb8000a739ee36c5
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-06-01 01:13:40 +0200

    88

--- front/src/comps/Todo.js
+++ front/src/comps/Todo.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export function Todo({todo}){
-	const {id, title, completed} = todo||{}
+	const {id, title, completed} = todo||{};
 	const h1 = <h1>todo-{title}</h1>
 	const text = completed ? <strike>{h1}</strike> : h1
 	return <div data-testid={`todo-${id}`}>{text}</div>
