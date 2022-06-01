016f9ebd59e97a74fdb4e9020b955f32ebbd710e
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-06-02 00:39:44 +0200

    921

--- front/src/Helper/Helper.js
+++
@@ -1,6 +0,0 @@
-export const multiply =(val1, val2)=> {
-	return val1 * val2
-}
-export const makeUpperCase =string=> {
-	return string.toUpperCase()
-}
--- front/src/Helper/Helper.test.js
+++
@@ -1,9 +0,0 @@
-import {multiply, makeUpperCase} from './Helper'
-
-test('multiply', ()=> {
-	expect(multiply(2,10)).toBe(20)
-})
-
-test('lowerCase', ()=>{
-	expect(makeUpperCase('yaro')).toBe('YARO')
-})
