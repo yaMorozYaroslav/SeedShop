3d6a6feafbdb830cff8f2c9d8c7dde56560165e4
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-06-09 20:48:29 -0300

    882

--- front/src/reducers/post.test.js
+++ front/src/reducers/post.test.js
@@ -1,1 +1,8 @@
 import {types} from '../../actions/types'
+import postRed from './postRed'
+
+describe('Posts Reducer', ()=> {
+	it('Should return default state', ()=> {
+		const newState = 
+	})
+})
