2aee69a1e656b31af8f4a2739fce0dabdbca374e
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-06-10 18:51:32 -0300

    373

---
+++ front/src/Comps/ListItem/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class ListItem extends React.Component {
+	render(){
+		const {title, desc} = this.props
+		return(
+			<div data-test='listItemComponent'>
+			  <h2 data-test='componentTitle'>{title}</h2>
+			  <div data-test='componentDesc'>{desc} </div>
+			
+			</div>
+			
+		)
+	}
+}
+ListItem.propTypes = {
+	title: PropTypes.string,
+	desc: PropTypes.string
+}
+export default ListItem
---
+++ front/src/Comps/ListItem/spec.test.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {shallow, configure} from 'enzyme'
+import {findByTestAtrr, checkProps} from '../../Tools/utils'
+import Adapter from 'enzyme-adapter-react-16'
+import ListItem from './index'
+configure({adapter: new Adapter()})
+
+describe('ListItem Component', ()=>{
+	describe('Checking PropTypes', ()=>{
+		it('Should NOT throw a warning', ()=> {
+			const expectedProps = {
+				title: 'Example Title',
+				desc: 'Some text'
+			}
+			const propsError = checkProps(ListItem, expectedProps)
+			expect(propsError).toBeUndefined()
+		})
+	})
+	describe('Component Renders', ()=> {
+		let wrapper
+		beforeEach(()=> {
+			const props = {
+				title: 'Example Title',
+				desc: 'Some text'
+			}
+			wrapper = shallow(<ListItem {...props}/>)
+		})
+		it('Should renders without error', ()=> {
+			const component = findByTestAtrr(wrapper, 'listItemComponent')
+			expect(component.length).toBe(1)
+		})
+		it('Should render a title', ()=> {
+			const title = findByTestAtrr(wrapper, 'componentTitle')
+			expect(title.length).toBe(1)
+		})
+		it('Should render a desc', ()=> {
+			const desc = findByTestAtrr(wrapper, 'componentDesc')
+			expect(desc.length).toBe(1)
+		})
+	})
+})
