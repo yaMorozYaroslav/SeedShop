f279f10475ef49d78f2988364869b23b457a8def
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-06-09 21:05:27 -0300

    37

--- front/src/reducers/post.test.js
+++ front/src/reducers/post.test.js
@@ -2,7 +2,20 @@ import {types} from '../../actions/types'
 import postRed from './postRed'
 
 describe('Posts Reducer', ()=> {
+
 	it('Should return default state', ()=> {
-		const newState = 
+		const newState = postRed(undefined, {})
+		expect(newState).toEqual([])
+	})
+
+	it('Should return new state if receiving type', ()=> {
+		const posts = [{title: 'Test 1'},
+		               {title: 'Test 2'},
+		               {title: 'Test 3'}]
+		const newState = postsReducer(undefined, {
+			type: types.GET_POSTS,
+			payload: posts
+		})
+		expect(newState).toEqual(posts)
 	})
 })
