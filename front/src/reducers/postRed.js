d227c325b635e7bc3edfead4878cbb8372a6e33c
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-06-09 21:11:23 -0300

    258

--- front/src/reducers/postRed.js
+++ front/src/reducers/postRed.js
@@ -1,5 +1,11 @@
 import {types} from '../../actions/types'
+import {type} from 'os'
 
 export default (state=[], action) => {
-	return null
+	switch(action.type){
+		case types.GET_POSTS:
+		     return action.payload
+		default:
+		     return state
+	}
 }
