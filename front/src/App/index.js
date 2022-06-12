57cdd1a105ebc08b6aedc98c374bcb52556f397e
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-06-12 19:51:02 -0300

    263

--- front/src/App/index.js
+++ front/src/App/index.js
@@ -2,9 +2,13 @@ import React from 'react'
 import {Paragraph} from './styled.js'
 import {SearchBox} from '../SearchBox'
 export function App(){
+	const [state, setState] = React.useState(false)
+	const handClick =()=> setState(item=>!item)
 	return (<>
 		      <SearchBox requestSearch={console.log}/>
-		      <Paragraph data-testid='Paragraph'/>
+		      <Paragraph onClick={handClick} data-testid='Paragraph'>
+		      {state?'Click':'Clock'}
+		      </Paragraph>
 
 
 		    </>)
--- front/src/App/spec.test.js
+++ front/src/App/spec.test.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Enzyme, {shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 import {render, fireEvent} from '@testing-library/react'
 import {App} from '../App'
+
+Enzyme.configure({adapter: new Adapter()})
 
 it('renders correctly', ()=> {
 	const {queryByTestId} = render(<App/>)
 	expect(queryByTestId('Paragraph')).toBeTruthy()
 })
+
