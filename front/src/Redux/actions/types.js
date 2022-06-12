8a15b8aaf6cb03b1b9bdc4a94884220147e002c5
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-06-12 10:14:07 -0300

    818

--- front/src/Redux/actions/types.js
+++
@@ -1,3 +0,0 @@
-export const types = {
-	GET_POSTS: 'GET_POSTS'
-}
--- front/src/Redux/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from 'redux'
-import posts from './postRed'
-
-export default combineReducers({
-	posts
-})
