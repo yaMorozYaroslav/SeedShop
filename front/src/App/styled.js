d0ff5a738634449fc7b315fb56cdc3e2babce131
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-06-12 19:58:42 -0300

    273

--- front/src/App/styled.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-export const Paragraph = styled.p`
-  font-size: 40px
-`
-export const App = styled.div`
-background-color: black`
--- front/src/SearchBox/box.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {render, fireEvent} from '@testing-library/react'
-import {SearchBox} from '../SearchBox'
-
-it('renders correctly', ()=> {
-	const {queryByTestId, queryByPlaceholderName} = render(<SearchBox />)
-	expect(queryByTestId('search-button')).toBeTruthy()
-})
