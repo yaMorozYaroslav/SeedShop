665ed1ed2bc3b2952fc231caefc081fdda97534a
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-06-12 10:33:49 -0300

    958

---
+++ front/src/App/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import {Paragraph} from './styled.js'
+export function App(){
+	return <Paragraph>Some</Paragraph>
+}
---
+++ front/src/App/styled.js
@@ -0,0 +1,5 @@
+import styled from 'styled-componets'
+
+export const Paragraph = styled.p`
+  font-size: 40px
+`
