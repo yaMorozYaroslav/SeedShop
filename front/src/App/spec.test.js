d100d94defe3cfff306fc2f26457fe366356976a
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-06-14 16:20:27 -0300

    559

--- front/src/App/spec.test.js
+++ front/src/App/spec.test.js
@@ -1,13 +1,27 @@
 import React from 'react'
-import {shallow} from 'enzyme'
+import {mount} from 'enzyme'
+import {useDispatch, Provider} from 'react-redux'
 import {App} from '../App'
 
+jest.mock('react-redux', () => {
+	const {Provider, useSelector} = jest.requireActual('react-redux')
 
-it('should render Text', ()=> {
-	const wrapper = shallow(<App />)
-	const article = wrapper.find('div')
-	expect(article).toBe('Text')
+	return {
+		useDispatch: jest.fn(),
+		useSelector,
+		Provider
+	}
+})
+it('should render App with Provider', ()=> {
+	const wrapper = mount(<Provider><App/></Provider>)
+	const globalStore = wrapper.find(Provider).prop('store')
 })
 
 //const article = wrapper.find(Footer)
 //expect(article.exists()).toBe(true)
+/*it('should render Text', ()=> {
+
+	const wrapper = shallow(<App />)
+	const article = wrapper.find('div')
+	expect(article).toBe('Text')
+})*/
