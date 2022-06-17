ca8bb6fd56b678adba6e91536b25989c5e5d43f7
Yaroslav Moroz <80963959+yaMorozYaroslav@users.noreply.github.com>
2022-06-16 21:04:34 -0300

    090

--- front/src/Redux/store.js
+++ front/src/Redux/store.js
@@ -3,7 +3,7 @@ import rootRed from './reducers'
 import thunk from 'redux-thunk'
 
 export const middle = [thunk]
-//Add some commits
+//Add some commits here
 export const storeWithMiddleware = applyMiddleware(...middle)(createStore)
 export const store = storeWithMiddleware(rootRed)*/
 import {configureStore} from '@reduxjs/toolkit'
