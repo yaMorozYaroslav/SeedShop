522cbb6946723d6ec23b9e83fe747006394d4463
Yaroslav Moroz <80963959+yaMorozYaroslav@users.noreply.github.com>
2022-06-23 22:09:27 -0300

    846

--- front/src/Redux/store.js
+++ front/src/Redux/store.js
@@ -3,7 +3,7 @@ import rootRed from './reducers'
 import thunk from 'redux-thunk'
 
 export const middle = [thunk]
-// How to find the job
+// I think 🤔
 export const storeWithMiddleware = applyMiddleware(...middle)(createStore)
 export const store = storeWithMiddleware(rootRed)*/
 import {configureStore} from '@reduxjs/toolkit'
