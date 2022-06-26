14039700212fa092cd445b8bd93462d015bb9069
Yaroslav Moroz <80963959+yaMorozYaroslav@users.noreply.github.com>
2022-06-26 11:17:22 -0300

    751

--- front/src/Redux/store.js
+++ front/src/Redux/store.js
@@ -3,7 +3,7 @@ import rootRed from './reducers'
 import thunk from 'redux-thunk'
 
 export const middle = [thunk]
-// I need buy an adapter 
+// I have a
 export const storeWithMiddleware = applyMiddleware(...middle)(createStore)
 export const store = storeWithMiddleware(rootRed)*/
 import {configureStore} from '@reduxjs/toolkit'
