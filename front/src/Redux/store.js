99a8d32d0d8a402679e473a21e9d645c88ee5652
Yaroslav Moroz <80963959+yaMorozYaroslav@users.noreply.github.com>
2022-06-27 23:44:51 -0300

    175

--- front/src/Redux/store.js
+++ front/src/Redux/store.js
@@ -3,7 +3,7 @@ import rootRed from './reducers'
 import thunk from 'redux-thunk'
 
 export const middle = [thunk]
-// I got a new task on my way 
+//New task
 export const storeWithMiddleware = applyMiddleware(...middle)(createStore)
 export const store = storeWithMiddleware(rootRed)*/
 import {configureStore} from '@reduxjs/toolkit'
