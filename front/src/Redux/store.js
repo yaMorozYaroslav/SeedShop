83ac70464695344f0f35846bd16be6d0b2af19d1
Yaroslav Moroz <80963959+yaMorozYaroslav@users.noreply.github.com>
2022-06-29 20:30:14 -0300

    7315

--- front/src/Redux/store.js
+++ front/src/Redux/store.js
@@ -3,7 +3,7 @@ import rootRed from './reducers'
 import thunk from 'redux-thunk'
 
 export const middle = [thunk]
-//Today is a day to install 
+//Today is a day to install Linux 
 export const storeWithMiddleware = applyMiddleware(...middle)(createStore)
 export const store = storeWithMiddleware(rootRed)*/
 import {configureStore} from '@reduxjs/toolkit'
