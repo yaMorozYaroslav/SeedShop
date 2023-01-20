afb444a607e886dac409226d07bc8b25b95aa600
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2023-01-20 17:37:09 -0400

    039

--- src/comps/Login/Login.js
+++ src/comps/Login/Login.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import axios from 'axios'
 
 export const Login =()=> {
+	const [loading, setLoading] = React.useState(false)
 	const [error, setError] = React.useState(false)
 	const [username, setUsername] = React.useState('')
 	const [password, setPassword] = React.useState('')
+	
+	const handleClick = async (e) => {
+		e.preventDefault()
+		setLoading(true)
+		try{
+			const {data} = axios.get('https://jsonplaceholder.typicode.com/users/1')
+			setUser(data)
+		}catch{
+			setError(true)
+			}
+		setLoading(false)
+		}
 	return (
   <div className='container'>
 	 <form>
@@ -16,7 +30,8 @@ export const Login =()=> {
 	       type='password' placeholder='password'
 	       value={password} onChange={(e)=> setPassword(e.target.value)}/>
 	       
-	   <button disabled={true}>Login</button>
+	   <button disabled={!username||!password} onClick={handleClick}>
+	      {loading?'please wait':'Login'}</button>
 	     <span data-testid='error' style={{visibility: error ? 'visible' : 'hidden'}}>
 	       Something went wrong.
 	     </span>
