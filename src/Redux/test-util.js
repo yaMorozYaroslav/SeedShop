b2199ea62ebaafff7b5f78230924d1b01c438f21
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2023-01-26 08:46:37 -0400

    461

--- src/Redux/test-util.js
+++ src/Redux/test-util.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {render} from '@testing-library/react'
 import {configureStore} from '@reduxjs/toolkit'
 import {Provider} from 'react-redux'
-import {setupStore} from './store'
+import {setupStore} from './store';
 
 export function renderWithProviders(
 ui,
