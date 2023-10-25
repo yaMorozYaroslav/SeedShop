210b94a76de46f054752dc90ef49e21f605fc4ee
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2023-10-25 12:05:51 -0300

    40

--- front/comps/AddForm/add-form.styled.js
+++ front/comps/AddForm/add-form.styled.js
@@ -10,6 +10,6 @@ export const Container = styled.div`
                        `
 export const Title = styled.h1`font-size:30px`
 export const Form = styled.form`font-size:24px`
-export const Input = styled.input`fontSize:22px;margin:7px;width:50%`
+export const Input = styled.input`fontSize:22px;margin:7px;width:40%`
 export const Description = styled.textarea`fontSize:22px;margin:7px;height:50px;marginLeft:45px;`
 export const Category = styled.select`fontSize:22px;margin:7px;cursor:pointer`
