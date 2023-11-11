8d2ee9a57367ce96286b68fc57a4d82c8a5b6b9c
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2023-11-10 22:41:42 -0400

    49

--- front/app/units/[id]/page.js
+++ front/app/units/[id]/page.js
@@ -1,25 +1,38 @@
 import {StyledLink} from './extra.styled.js'
 
-export const dynamicParams = false
+export const dynamicParams = true
 
 export async function generateStaticParams(){
   const items = await fetch('http://localhost:5000/items?search=').then((res) => res.json())
   const seeds = await fetch('http://localhost:5000/seeds?search=').then((res) => res.json())
-    return items.data.map((item) => ({id: item._id}))
+  const arrItems = items.data.map((item) => ({id: item._id}))
+  const arrSeeds = seeds.data.map((seed) => ({id: seed._id}))
+   // return items.data.map((item) => ({id: item._id}))
+    return [...arrItems, ...arrSeeds]
 	}
-async function getItem(source) {
+ async function getItem(source) {
   
-  const item = 
-    await fetch(`http://localhost:5000/items/${source}`)
+   const item = await fetch(`http://localhost:5000/items/${source}`)
                                               .then((res) => res.json())
-     return item}
+     if(item){return item}
+     if(!item){return false}
 
+       }
+ async function getSeed(source) {
+  
+   const seed = await fetch(`http://localhost:5000/seeds/${source}`)
+                                              .then((res) => res.json())
+     if(seed){return seed}
+     if(!seed){return false}
+
+       }
 export default async function Item({params}){
 	
-	const item = await getItem(params.id)
-	
+	//const unit = await getItem(params.id)
+	//const seed = await getUnit(params.id)
+	const item = await getUnit(params.id)
 	return <><p>{item._id}, price - {item.price}</p>
-	
+	         <p>{item.title}</p>
 	<StyledLink href={'/'}>Back To Menu</StyledLink>
 	</>
 	}
