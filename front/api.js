64b792bc9e8261a5dc71ee9391ea2f13e011369d
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2023-11-23 19:52:24 -0400

    67

--- front/api.js
+++ front/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({baseURL:'http://localhost:5000'})
+const API = axios.create({baseURL:'https://seed-shop-back-78049b8c30bb.herokuapp.com'})
 
 API.interceptors.request.use((req)=>{
     if(localStorage.getItem('profile')){
