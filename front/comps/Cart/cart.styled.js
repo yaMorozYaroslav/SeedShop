78819fd4544bf703bfc524937ce71d6b22836d74
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2023-12-11 14:29:04 -0400

    87

--- front/comps/Cart/cart.styled.js
+++ front/comps/Cart/cart.styled.js
@@ -8,36 +8,56 @@ export const Container = styled.div`margin-top:20px;
 export const CartList = styled.div`width: 60%;
                                    margin-left:20%;margin-bottom:30px;
                         @media (max-width: 1000px) {margin-left:15%;width:70%}
-                        @media (max-width: 800px) {margin-left:5%;width:90%}`
+                        @media (max-width: 800px) {margin-left:5%;width:90%}
+                        @media (max-width: 600px) {margin-left:3%;width:94%;}`
 export const Thing = styled.section`display:grid;
                                     grid-template-columns: repeat(6, 1fr);
                                     grid-template-rows: 100px 100px 100px;
                                     border:2px solid black; font-size:23px;
                                     padding: 15px; height:180px; margin:5px;
-                     @media (max-width: 600px){height:310px; }`
-export const Number = styled.label`grid-row:2;margin-left:-5px;margin-top:25px;
+                     @media (max-width: 600px){height:310px;}
+                     @media (max-width: 400px) {
+                             height: 400px;
+                             grid-template-columns: repeat(7, 1fr);
+                             grid-template-rows: 100px 100px 100px 100px 100px;}`
+export const Number = styled.label`grid-column:1;grid-row:2;
+                                   margin-left:-5px;margin-top:25px;
                                    font-size:32px;padding:10px;
                       @media (max-width: 600px){margin-top:30px;}`
 export const StyledImage = styled(Image)`grid-row:1;width:120px;height:120px;
                                          margin:10px;border: 1px solid black;
-                           @media (max-width:400px) {}`
-export const Title = styled.p`grid-column:2/5;grid-row:1;
-                     @media (max-width: 600px) {grid-column:2/7;}`
-export const Quantity = styled.p`grid-column:2/5;grid-row:2;margin-top:-10px;
-                                 background: lightgreen;padding:15px;width:80%;height:45px;
+                           @media (max-width:600px) {margin-left:0px;}
+                           @media (max-width:400px) {grid-column:3/8;
+                                                     width:150px;height:150px;}`
+export const Title = styled.h1`grid-column:2/5;grid-row:1;
+                              width:90%;font-size:25px;margin-top:20px;
+                     @media (max-width: 600px) {grid-column:3/8;}
+                     @media (max-width: 400px) {grid-column:2/8;grid-row:3;
+                                                margin-top:-25px;}`
+export const Quantity = styled.p`grid-column:2/5;grid-row:2;
+                                 margin-top:-10px;
+                                 background: lightgreen;padding:15px;
+                                 width:80%;padding-top:-20px;
                         @media (max-width: 600px) {grid-column:2/7;
-                                                   margin-top:20px;
-                                                   height:70px;width:83%}`
+                                                   margin-top:20px;padding-top:-5px;
+                                                   height:70px;width:83%}
+                        @media (max-width: 400px) {grid-column:1/8;grid-row:3;
+                                                   height:55px;padding:10px;
+                                                   margin-top:40px;margin-left:10px;}`
 export const Butts = styled.section`grid-column:5/7;grid-row:1/3;
                                     border-style:dotted;
                                     width:100%;height:175px;
                                     text-align:right;
                                     margin-top:0px;
                      @media (max-width: 1000px) {display:block;}
-                     @media (max-width: 600px) {grid-column:1/7;grid-row:3;
-                                                height:58px;margin-top:50px;}`
-export const ThingButt= styled.button`font-size:22px;width:100%;;height:58px;
-                        @media (max-width: 600px) {width:33.3%;font-size:20px;}`
+                     @media (max-width: 600px) {grid-column:1/8;grid-row:3;
+                                                height:58px;margin-top:50px;}
+                     @media (max-width: 400px) {grid-row:4;height:70px;
+                                                margin-top:30px;margin-left:-5px;
+                                                text-align:center;border:none;}`
+export const ThingButt= styled.button`font-size:22px;width:100%;height:58px;
+                        @media (max-width: 600px) {width:33.3%;font-size:23px;}
+                        @media (max-width: 400px) {width:50%;height:35px;}`
 export const MailForm = styled.form`position:fixed;
                                     top:100px;left:30%;
                                     width:40%;
