8ef9dc76c197a44699d58fa032a7ac3f9424b966
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2023-12-10 22:00:34 -0400

    84

--- front/comps/Cart/Cart.js
+++ front/comps/Cart/Cart.js
@@ -50,21 +50,27 @@ export const Cart =()=> {
 		}
 
 	return <S.Container>	
-	         <S.CartList>{cartItems.map((item,index)=>
-		              <S.Thing key={item._id}> 
-		    <S.Number>{index+1}.</S.Number>
-            <S.StyledImage alt='' src={item.photo&&item.photo.length
+	
+	    <S.CartList>{cartItems.map((item,index)=>
+				 
+		   <S.Thing key={item._id}> 
+		      <S.Number>{index+1}.</S.Number>
+              <S.StyledImage alt='' src={item.photo&&item.photo.length
 				                      ?item.photo:'./next.svg'}
-                              width={100} height={100} priority={true}/>
-		                <S.Title>{item.title} </S.Title>
+                              width={0} height={0} priority={true}/>
+		        <S.Title>{item.title} </S.Title><br/>
 		               
-		                <S.Quantity>price: {item.price}/
+		        <S.Quantity>price: {item.price}/<br/>
 		                       quantity: {item.quantity}</S.Quantity>
-		      <S.Butts> <S.ThingButt onClick={()=>increase(item._id)}>increase</S.ThingButt> 
+		                       
+		              <S.Butts> 
+		                <S.ThingButt onClick={()=>increase(item._id)}>increase</S.ThingButt> 
 		                <S.ThingButt onClick={()=>decrease(item._id)}>decrease</S.ThingButt>
-		                <S.ThingButt onClick={()=>remover(item._id)}>remove</S.ThingButt> </S.Butts>                         
-		                                                </S.Thing>)}
-		      </S.CartList>
+		                <S.ThingButt onClick={()=>remover(item._id)}>remove</S.ThingButt>
+		              </S.Butts>                         
+		   </S.Thing>)}
+	    </S.CartList>
+		      
 		    {open && <S.MailForm ref={form}
 	                    onSubmit={sendEmail}>
 	  
@@ -77,11 +83,14 @@ export const Cart =()=> {
 	       <br />
 	             <S.Button type='submit'>Place The Order</S.Button>
 	             <S.Button onClick={()=>setOpen(false)}>CloseForm</S.Button>
-	         </S.MailForm>}
-	           <p>total: {counter()}</p>
-		         <S.Button onClick={()=>setOpen(true)}>Order Items</S.Button>
-		         <S.Button onClick={cleaner}>ClearCart</S.Button><br/>
+	                 </S.MailForm>}
+	                 
+	         <S.CartButts>
+	            <p>total: {counter()}</p>
+		          <S.Button onClick={()=>setOpen(true)}>Order Items</S.Button>
+		          <S.Button onClick={cleaner}>ClearCart</S.Button><br/>
 		           <S.StyledLink className='styledLink' href='/'>
 		                                       To Menu</S.StyledLink>
+		     </S.CartButts>
 	       </S.Container>
 	}
--- front/comps/Cart/cart.styled.js
+++ front/comps/Cart/cart.styled.js
@@ -3,25 +3,41 @@ import styled from 'styled-components'
 import Link from 'next/link'
 import Image from 'next/image'
 
-export const Container = styled.div`text-align:center;`
-export const CartList = styled.div`text-align: left; width: 60%;
+export const Container = styled.div`margin-top:20px;
+                         @media (max-width:600px) {margin-top:45px;}`
+export const CartList = styled.div`width: 60%;
                                    margin-left:20%;margin-bottom:30px;
-                        @media (max-width: 1000px) {margin-left:15%;width:70%}`
-export const Button = styled.button`font-size:25px;margin:5px;margin-bottom:20px;
-                                    padding:5px;`
-export const StyledLink = styled(Link)`font-size: 24px;padding: 10px;
-                                       margin-bottom:10px; border:1px solid olive; 
-                                       color:black;`
-export const Thing = styled.section`border:1px solid black; font-size:23px;
-                                    padding: 20px; margin:5px;`
-export const Number = styled.label`border-style:groove;font-size:30px;padding:5px;`
-export const StyledImage = styled(Image)`margin:10px;border: 1px solid black;`
-export const Title = styled.label`position: relative;bottom: 80px;left:20px;`
-export const Quantity = styled.label`background: lightgreen;padding:10px;margin-left:-100px;`
-export const Butts = styled.section`border-style:dotted;left;width:80%;margin-left:20%;
-                                    text-align:right;margin-top:20px;
-                     @media (max-width: 1000px) {font-size:40px;}`
-export const ThingButt= styled.button`font-size:22px;`
+                        @media (max-width: 1000px) {margin-left:15%;width:70%}
+                        @media (max-width: 800px) {margin-left:5%;width:90%}`
+export const Thing = styled.section`display:grid;
+                                    grid-template-columns: repeat(6, 1fr);
+                                    grid-template-rows: 100px 100px 100px;
+                                    border:2px solid black; font-size:23px;
+                                    padding: 15px; height:180px; margin:5px;
+                     @media (max-width: 600px){height:310px; }`
+export const Number = styled.label`grid-row:2;margin-left:-5px;margin-top:25px;
+                                   font-size:32px;padding:10px;
+                      @media (max-width: 600px){margin-top:30px;}`
+export const StyledImage = styled(Image)`grid-row:1;width:120px;height:120px;
+                                         margin:10px;border: 1px solid black;
+                           @media (max-width:400px) {}`
+export const Title = styled.p`grid-column:2/5;grid-row:1;
+                     @media (max-width: 600px) {grid-column:2/7;}`
+export const Quantity = styled.p`grid-column:2/5;grid-row:2;margin-top:-10px;
+                                 background: lightgreen;padding:15px;width:80%;height:45px;
+                        @media (max-width: 600px) {grid-column:2/7;
+                                                   margin-top:20px;
+                                                   height:70px;width:83%}`
+export const Butts = styled.section`grid-column:5/7;grid-row:1/3;
+                                    border-style:dotted;
+                                    width:100%;height:175px;
+                                    text-align:right;
+                                    margin-top:0px;
+                     @media (max-width: 1000px) {display:block;}
+                     @media (max-width: 600px) {grid-column:1/7;grid-row:3;
+                                                height:58px;margin-top:50px;}`
+export const ThingButt= styled.button`font-size:22px;width:100%;;height:58px;
+                        @media (max-width: 600px) {width:33.3%;font-size:20px;}`
 export const MailForm = styled.form`position:fixed;
                                     top:100px;left:30%;
                                     width:40%;
@@ -29,5 +45,11 @@ export const MailForm = styled.form`position:fixed;
                                     text-align:center;
                                     background: green;`
 export const Input = styled.input`font-size:25px`
+export const Button = styled.button`font-size:25px;margin:5px;margin-bottom:20px;
+                                    padding:5px;`
+export const StyledLink = styled(Link)`font-size: 24px;padding: 10px;
+                                       margin-bottom:10px; border:1px solid olive; 
+                                       color:black;`
+export const CartButts = styled.section`text-align:center;`
 
 
