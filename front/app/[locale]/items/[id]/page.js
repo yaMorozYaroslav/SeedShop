cefaee251400f958e70bc6cf0ef5af6b10e7806a
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2023-12-20 19:35:05 -0400

    105

--- front/app/[locale]/items/[id]/page.js
+++ front/app/[locale]/items/[id]/page.js
@@ -2,13 +2,13 @@ import {Single} from '../../../../comps/Single/Single'
 
 export const dynamicParams = true
 
-export async function generateStaticParams(){
-  const items = await fetch(
-    'https://seed-shop-back-78049b8c30bb.herokuapp.com/items?category=')
-                                        .then((res) => res.json())
-  const arrItems = items.data.map((item) => ({id: item._id}))
-    return arrItems
-	}
+//~ export async function generateStaticParams(){
+  //~ const items = await fetch(
+    //~ 'https://seed-shop-back-78049b8c30bb.herokuapp.com/items?category=')
+                                        //~ .then((res) => res.json())
+  //~ const arrItems = items.data.map((item) => ({id: item._id}))
+    //~ return arrItems
+	//~ }
  async function getItem(source) {
   
    const item = await fetch(
--- front/app/[locale]/seeds/[id]/page.js
+++ front/app/[locale]/seeds/[id]/page.js
@@ -2,13 +2,13 @@ import {Single} from '../../../../comps/Single/Single'
 
 export const dynamicParams = true
 
-export async function generateStaticParams(){
-  const seeds = await fetch(
-    'https://seed-shop-back-78049b8c30bb.herokuapp.com/seeds?category=&type=')
-                                        .then((res) => res.json())
-  const arrSeeds = seeds.data.map((seed) => ({id: seed._id}))
-    return  arrSeeds
-	}
+//~ export async function generateStaticParams(){
+  //~ const seeds = await fetch(
+    //~ 'https://seed-shop-back-78049b8c30bb.herokuapp.com/seeds?category=&type=')
+                                        //~ .then((res) => res.json())
+  //~ const arrSeeds = seeds.data.map((seed) => ({id: seed._id}))
+    //~ return  arrSeeds
+	//~ }
  async function getSeed(source) {
   
    const seed = await fetch(
